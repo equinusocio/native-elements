3ce8948aa93da79352140ac7d7f4b425e966876f
Mattia Astorino <astorino.mattia@gmail.com>
2019-12-26 23:20:46 +0100

    Update congis

---
+++ src/configs.js
@@ -0,0 +1,5 @@
+export {coreConfig} from './elements/native-elements/config';
+export {buttonConfig} from './elements/ne-button/config';
+export {checkradioConfig} from './elements/ne-checkradio/config';
+export {selectConfig} from './elements/ne-select/config';
+export {textfieldConfig} from './elements/ne-textfield/config';
