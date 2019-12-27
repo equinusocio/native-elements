f6488e36a5a8066753edcae910a39183e04c02c2
Mattia Astorino <astorino.mattia@gmail.com>
2019-12-27 11:24:23 +0100

    Clea files

--- src/configs.js
+++
@@ -1,5 +0,0 @@
-export {coreConfig} from './elements/native-elements/config';
-export {buttonConfig} from './elements/ne-button/config';
-export {checkradioConfig} from './elements/ne-checkradio/config';
-export {selectConfig} from './elements/ne-select/config';
-export {textfieldConfig} from './elements/ne-textfield/config';
