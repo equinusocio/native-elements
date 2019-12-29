5587cea555cb97ee8ad369a429a0588170247bcd
Mattia Astorino <astorino.mattia@gmail.com>
2019-12-29 16:42:52 +0100

    Update buttons

--- src/elements/ne-button/config.js
+++ src/elements/ne-button/config.js
@@ -2,21 +2,18 @@ module.exports = {
   /* Border API */
   '--buttonBorder': 'var(--ne-button-border, 0)',
   '--buttonHoverBorder': 'var(--ne-button-hover-border, 0)',
-  '--buttonFocusBorder': 'var(--ne-button-focus-border, 0)',
   '--buttonActiveBorder': 'var(--ne-button-active-border, 0)',
   '--buttonDisabledBorder': 'var(--ne-button-disabled-border, 0)',
 
   /* Background API */
   '--buttonBackground': 'var(--ne-button-background, hsl(235, 100%, 60%))',
   '--buttonHoverBackground': 'var(--ne-button-hover-background, hsl(233, 64%, 52%))',
-  '--buttonFocusBackground': 'var(--ne-button-focus-background, hsl(233, 64%, 52%))',
   '--buttonActiveBackground': 'var(--ne-button-active-background, hsl(233, 64%, 32%))',
   '--buttonDisabledBackground': 'var(--ne-button-disabled-background, hsl(233, 34%, 96%))',
 
   /* Foreground API */
   '--buttonForeground': 'var(--ne-button-foreground, #FFF)',
   '--buttonHoverForeground': 'var(--ne-button-hover-foreground, #FFF)',
-  '--buttonFocusForeground': 'var(--ne-button-focus-foreground, #FFF)',
   '--buttonActiveForeground': 'var(--ne-button-active-foreground, #FFF)',
   '--buttonDisabledForeground': 'var(--ne-button-disabled-foreground, hsl(233, 24%, 72%))',
 
--- src/elements/ne-select/config.js
+++ src/elements/ne-select/config.js
@@ -23,7 +23,7 @@ module.exports = {
   '--selectBorderRadius': 'var(--ne-select-radius, 5px)',
   '--selectWidth': 'var(--ne-select-width, 100%)',
   '--selectPadding': 'var(--ne-select-padding, 0.5em)',
-  '--selectArrowIcon': 'var(--ne-select-arrow-icon, url(\'data:image/svg+xml;utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"> <path fill="none" stroke="#111" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" d="M7 9l5-5 5 5m0 6l-5 5-5-5" class="nc-icon-wrapper"/></svg>\'))',
-  '--selectArrowSize': 'var(--ne-select-arrow-size, 0.9rem)',
+  '--selectArrowIcon': 'var(--ne-select-arrow-icon, url(\'data:image/svg+xml;utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><g stroke-linecap="round" stroke-linejoin="round" stroke-width="2" fill="none" stroke="hsl(235, 100%, 60%)" stroke-miterlimit="10"><path d="M7 8l5-5 5 5M17 16l-5 5-5-5"/></g></svg>\'))',
+  '--selectArrowSize': 'var(--ne-select-arrow-size, 1rem)',
   '--selectShadow': 'var(--ne-select-shadow, 0 1px 2px rgba(0, 0, 0, 0.06))',
 };
