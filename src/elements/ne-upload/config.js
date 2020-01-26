9c0abd53071ad609bd036af9c4dc2b9b9e8c459c
Mattia Astorino <astorino.mattia@gmail.com>
2020-01-26 23:25:50 +0100

    clean ne-upload props

--- src/elements/ne-upload/config.js
+++ src/elements/ne-upload/config.js
@@ -2,8 +2,4 @@ module.exports = {
   /* Foreground */
   '--uploadLabelWeight': 'var(--ne-upload-label-weight, 700)',
   '--uploadLabelFontSize': 'var(--ne-upload-label-font-size, 1.2rem)',
-
-  /* Misc API */
-  '--uploadOutlineColor': 'var(--ne-upload-outline-color, hsl(233, 64%, 85%))',
-  '--uploadOutlineWidth': 'var(--ne-upload-outline-width, 0.2rem)',
 };
