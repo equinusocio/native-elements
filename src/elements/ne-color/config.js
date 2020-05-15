ab0c8cd6a67889492d6bc460574f4fb417b855d2
Mattia Astorino <astorino.mattia@gmail.com>
2020-05-15 16:29:58 +0200

    fix(color): fix ne-color sizes and add --ne-color-width and --ne-color-height props

--- src/elements/ne-color/config.js
+++ src/elements/ne-color/config.js
@@ -5,4 +5,6 @@ module.exports = {
   /* Misc API */
   '--colorBorderRadius': 'var(--ne-color-radius, 100%)',
   '--colorShadow': 'var(--ne-color-shadow, 0 1px 2px rgba(0, 0, 0, 0.16))',
+  '--colorWidth': 'var(--ne-color-width, 24px)',
+  '--colorHeight': 'var(--ne-color-height, var(--ne-color-width, 24px))',
 };
