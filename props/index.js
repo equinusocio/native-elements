3e4536f875a25fd9e01d49f5ca968a263473fe5d
Mattia Astorino <astorino.mattia@gmail.com>
2020-05-22 22:11:44 +0200

    chore: update scripts

---
+++ props/index.js
@@ -0,0 +1,13 @@
+const CORE_PROPS = require('./core');
+const BUTTON_PROPS = require('./button');
+const CHECKRADIO_PROPS = require('./checkradio');
+const CODE_PROPS = require('./code');
+const COLOR_PROPS = require('./color');
+
+module.exports = [
+  ...CORE_PROPS,
+  ...BUTTON_PROPS,
+  ...CHECKRADIO_PROPS,
+  ...CODE_PROPS,
+  ...COLOR_PROPS
+]
