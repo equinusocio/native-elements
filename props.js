08b04ecd9be83e9598749d377acfdc188d7a023c
Mattia Astorino <astorino.mattia@gmail.com>
2020-05-22 22:07:11 +0200

    chore: add colr props

--- props.js
+++ props.js
@@ -2,10 +2,12 @@ const CORE_PROPS = require('./props/core');
 const BUTTON_PROPS = require('./props/button');
 const CHECKRADIO_PROPS = require('./props/checkradio');
 const CODE_PROPS = require('./props/code');
+const COLOR_PROPS = require('./props/color');
 
 module.exports = [
   ...CORE_PROPS,
   ...BUTTON_PROPS,
   ...CHECKRADIO_PROPS,
-  ...CODE_PROPS
+  ...CODE_PROPS,
+  ...COLOR_PROPS
 ]
