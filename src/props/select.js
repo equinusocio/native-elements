377ec7222cb84b35ddb632fbaf33dccd19c43f52
Mattia Astorino <astorino.mattia@gmail.com>
2020-05-23 22:30:32 +0200

    chore: fix inheritance for select

--- src/props/select.js
+++ src/props/select.js
@@ -81,16 +81,16 @@ module.exports = [
   }
   ,{
     "name": "--ne-select-hover-foreground",
-    "tokenValue": "var(--ne-select-hover-foreground, hsl(0, 0%, 13%))",
-    "defaultValue": "hsl(0, 0%, 13%)",
+    "tokenValue": "var(--ne-select-hover-foreground, inherit)",
+    "defaultValue": "",
     "domain": "elements",
     "type": "color",
     "element": "select"
   }
   ,{
     "name": "--ne-select-focus-foreground",
-    "tokenValue": "var(--ne-select-focus-foreground, hsl(0, 0%, 13%))",
-    "defaultValue": "hsl(0, 0%, 13%)",
+    "tokenValue": "var(--ne-select-focus-foreground, inherit)",
+    "defaultValue": "",
     "domain": "elements",
     "type": "color",
     "element": "select"
@@ -137,7 +137,7 @@ module.exports = [
   }
   ,{
     "name": "--ne-select-icon",
-    "tokenValue": "var(--ne-select-icon, url(data:image/svg+xml;utf-8,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24'><path d='M7 8l5-5 5 5m0 8l-5 5-5-5' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' fill='none' stroke='currentColor' stroke-miterlimit='10'/></svg>)))",
+    "tokenValue": "var(--ne-select-icon, url(data:image/svg+xml;utf-8,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24'><path d='M7 8l5-5 5 5m0 8l-5 5-5-5' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' fill='none' stroke='currentColor' stroke-miterlimit='10'/></svg>))",
     "defaultValue": "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24'><path d='M7 8l5-5 5 5m0 8l-5 5-5-5' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' fill='none' stroke='currentColor' stroke-miterlimit='10'/></svg>",
     "domain": "elements",
     "type": "text",
