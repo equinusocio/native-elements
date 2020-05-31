c61487869ba3f0e1cd862fc3440dd6e53d25b432
Mattia Astorino <astorino.mattia@gmail.com>
2020-05-31 13:13:17 +0200

    fix: remove unused upload props

--- src/props/upload.js
+++ src/props/upload.js
@@ -1,18 +1,1 @@
-module.exports = [
-  {
-    'name': '--ne-upload-label-weight',
-    'tokenValue': 'var(--ne-upload-label-weight, 700)',
-    'defaultValue': '700',
-    'domain': 'elements',
-    'type': 'text',
-    'element': 'upload'
-  }
-  ,{
-    'name': '--ne-upload-label-font-size',
-    'tokenValue': 'var(--ne-upload-label-font-size, 1.2rem)',
-    'defaultValue': '1.2rem',
-    'domain': 'elements',
-    'type': 'unit',
-    'element': 'upload'
-  }
-];
+module.exports = [];
