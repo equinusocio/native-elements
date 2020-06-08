c37d82df3e3f0807ff3219c7270a4f19fa5873bc
Mattia Astorino <astorino.mattia@gmail.com>
2020-06-08 19:22:41 +0200

    chore: clean demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,16 +1,16 @@
 import 'what-input';
 
-const html = document.documentElement;
-const themeSwitcher = document.getElementById('themeSwitcher');
+// const html = document.documentElement;
+// const themeSwitcher = document.getElementById('themeSwitcher');
 
-themeSwitcher.addEventListener('change', () => {
+// themeSwitcher.addEventListener('change', () => {
 
-  if (themeSwitcher.value === 'auto') {
-    delete html.dataset.theme;
-  } else if (themeSwitcher.value === 'light') {
-    html.dataset.theme = 'light';
+//   if (themeSwitcher.value === 'auto') {
+//     delete html.dataset.theme;
+//   } else if (themeSwitcher.value === 'light') {
+//     html.dataset.theme = 'light';
 
-  } else if (themeSwitcher.value === 'dark') {
-    html.dataset.theme = 'dark';
-  }
-});
+//   } else if (themeSwitcher.value === 'dark') {
+//     html.dataset.theme = 'dark';
+//   }
+// });
